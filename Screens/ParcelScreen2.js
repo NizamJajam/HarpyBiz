2ff9e858fe349a7f6e180c32c64607bfaa73eaf4
NizamJajam <58840684+NizamJajam@users.noreply.github.com>
2021-11-29 16:52:29 +0800

    Update 29/11/2021

--- Screens/ParcelScreen2.js
+++ Screens/ParcelScreen2.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, Dimensions, TextInput, TouchableOpacity, Image, KeyboardAvoidingView, ScrollView } from 'react-native';
 const windowHeight = Dimensions.get('window').height;
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 export default class ParcelScreen2 extends React.Component {
     constructor(props) {
@@ -10,8 +11,32 @@ export default class ParcelScreen2 extends React.Component {
             harpyCR: "Mohamed Amrin Bin Abdul Mansor",
             phoneNumber: "010 357 0729",
             address: "No 23, Lorong 3, Taman Sempaul, 88000 Kota Kinabalu Sabah",
-            name:'Mohd Iswan Bin Mokhtar'
+            name: 'Mohd Iswan Bin Mokhtar',
+            quantity: 1,
+            paymentMethod: 'Maybank',
+            deliveryLocation: 'No 7, Lorong 7, Jalan Tujuh, 7777, Kota Kinabalu Sabah',
+            subtotal: 'RM 16.00',
+            tax: 'RM 0.00',
+            accountNumber: "***0729"
         }
+    }
+    addNumber() {
+        const number = this.state.quantity;
+        this.setState({
+            quantity: number + 1
+        })
+    }
+    minusNumber() {
+        const number = this.state.quantity;
+        if (number == 1) {
+
+        }
+        else {
+            this.setState({
+                quantity: number - 1
+            })
+        }
+
     }
     render() {
         return (
@@ -27,27 +52,65 @@ export default class ParcelScreen2 extends React.Component {
 
                     </TouchableOpacity>
                     <View style={styles.bottomView}>
-                        {/* <TextInput placeholder="Description" placeholderTextColor="grey" style={styles.textInput} />
-                        <TextInput placeholder="Category" placeholderTextColor="grey" style={styles.textInput} />
-                        <TextInput placeholder="Phone Number" placeholderTextColor="grey" style={styles.textInput} keyboardType="phone-pad"/> */}
+
                         <View style={styles.sections}>
+                            <Text style={styles.text}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</Text>
                             <Text style={styles.title}>Category</Text>
                         </View>
                         <View style={styles.sections}>
                             <Text style={styles.title}>{this.state.name}</Text>
+                            <Text style={styles.text}>+011-3646 3237</Text>
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center' }}>
+                                <View style={{ flexDirection: 'row' }}>
+                                    <TouchableOpacity style={styles.smallButton}><Text style={styles.text}>-</Text></TouchableOpacity>
+                                    <Text style={{ fontSize: 20, color: "black" }}>  M  </Text>
+                                    <TouchableOpacity style={styles.smallButton}><Text style={styles.text}>+</Text></TouchableOpacity>
+                                </View>
+                                <View style={{ flexDirection: 'row' }}>
+                                    <TouchableOpacity style={styles.smallButton} onPress={() => { this.minusNumber() }}><Text style={styles.text}>-</Text></TouchableOpacity>
+                                    <Text style={{ fontSize: 20, color: "black" }}>  {this.state.quantity}  </Text>
+                                    <TouchableOpacity style={styles.smallButton} onPress={() => { this.addNumber() }}><Text style={styles.text}>+</Text></TouchableOpacity>
+                                </View>
+                                <View>
+
+                                </View>
+                            </View>
                         </View>
                         <View style={styles.sections}>
                             <Text style={styles.title}>Delivery Location</Text>
-                            <Text style={styles.text}>Address</Text>
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', }}>
+                                <View style={styles.iconContainer}>
+                                    <Icon name="compass" color='#FAAF40' size={20} />
+                                </View>
+                                <Text style={styles.smallText}>
+                                    {this.state.deliveryLocation}
+                                </Text>
+                            </View>
                         </View>
                         <View style={styles.sections}>
                             <Text style={styles.title}>Payment Method</Text>
-                            <Text style={styles.text}>Cash on Delivery</Text>
+                            <View style={{ flexDirection: 'row', alignItems: 'center', }}>
+                                <View style={styles.iconContainer}>
+                                    <Icon name="cc-visa" color='#FAAF40' size={20} />
+                                </View>
+                                <Text style={styles.smallText}>
+                                    {this.state.paymentMethod} {"\n"}
+                                    {this.state.accountNumber}
+                                </Text>
+                            </View>
                         </View>
                         <View style={styles.sections}>
                             <Text style={styles.title}>Order Info</Text>
-                            <Text style={styles.text}>Subtotal</Text>
-                            <Text style={styles.text}>Tax</Text>
+                            <View style={styles.subtotalContainer}>
+                                <View style={styles.subtotal}>
+                                    <Text style={styles.greyText}>Subtotal</Text>
+                                    <Text style={styles.greyText}>Tax</Text>
+                                </View>
+                                <View style={styles.subtotal2}>
+                                    <Text style={styles.greyText}>{this.state.subtotal}</Text>
+                                    <Text style={styles.greyText}>{this.state.tax}</Text>
+                                </View>
+                            </View>
                         </View>
 
 
@@ -72,7 +135,7 @@ export default class ParcelScreen2 extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        width: '100%'
+        width: '100%',
     },
     main: {
         flex: 1,
@@ -80,8 +143,7 @@ const styles = StyleSheet.create({
         backgroundColor: "white",
         justifyContent: 'space-evenly',
         alignItems: 'center',
-        height: windowHeight / 2
-
+        height: windowHeight / 2,
     },
     topView: {
         height: 300,
@@ -91,16 +153,23 @@ const styles = StyleSheet.create({
         backgroundColor: '#F2F2F2',
         borderBottomStartRadius: 40,
         borderBottomEndRadius: 40,
-        elevation: 5
-
-
+        elevation: 5,
     },
     bottomView: {
-        height: 500,
+        paddingTop: 20,
+        height: 620,
+        paddingHorizontal: 10,
         justifyContent: 'space-around',
         alignItems: 'center',
         width: '100%',
-
+    },
+    smallButton: {
+        backgroundColor: '#F2F2F2',
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     pictureFrame: {
         width: '80%',
@@ -110,19 +179,55 @@ const styles = StyleSheet.create({
         borderRadius: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        overflow: 'hidden'
-
+        overflow: 'hidden',
     },
     sections: {
         width: '85%',
-        height: '15%',
+        height: '20%',
+
     },
     title: {
         color: 'black',
-        fontSize: 15,
-        fontWeight: 'bold'
+        fontSize: 19,
+        fontWeight: 'bold',
+        paddingBottom: 10
     },
     text: {
         color: 'black',
+        fontWeight: 'bold',
+        paddingBottom: 10,
+    },
+    greyText: {
+        color: 'grey'
+    },
+    smallText: {
+        color: 'black',
+        left: 10,
+
+    },
+    iconContainer: {
+        backgroundColor: '#F2F2F2',
+        width: 40,
+        height: 40,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+    subtotalContainer: {
+        width: '100%',
+        height: '50%',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    subtotal: {
+        flex: 1,
+        justifyContent: 'space-evenly',
+        alignItems: 'flex-start'
+    },
+    subtotal2: {
+        flex: 1,
+        justifyContent: 'space-evenly',
+        alignItems: 'flex-end'
     },
 })
